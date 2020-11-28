17d148bfe90ee099d5aebcbe0db4bdca331e8896
Shengy <shengy90@gmail.com>
2020-11-28 13:22:50 +0000

    test files

--- definitions/test_jsons.py
+++ definitions/test_jsons.py
@@ -6,8 +6,8 @@ import json
 
 current_file = os.path.realpath(__file__)
 parent_dir = str(Path(current_file).parent.parent)
-test_default_order_json_path = os.path.join(parent_dir, 'bin', 'test_default_orders.json')
-test_stock_json_path = os.path.join(parent_dir, 'bin', 'test_stock.json')
+test_default_order_json_path = os.path.join(parent_dir, 'bin', 'test_files', 'test_default_orders.json')
+test_stock_json_path = os.path.join(parent_dir, 'bin', 'test_files', 'test_stock.json')
 
 with open(test_default_order_json_path) as json_file:
     TEST_DEFAULT_ORDERS_DICT = json.load(json_file)
